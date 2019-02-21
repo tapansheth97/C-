70ce6bf1bce8c275be5b36a5d812ba64ce5b547e
tapan <15ce151@charusat.edu.in>
2019-02-21 09:13:21 +0530

    first commit

---
+++ Assignment-1/Assignment-1/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Employee
+    {
+        private string Id;
+        private string Name;
+        private string DepartmentName;
+
+        public Employee(string id, string name, string departmentName)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.DepartmentName = departmentName;
+        }
+
+        public string GetId()
+        {
+            OnMethodCall("\nGetId() method called");
+            return this.Id;
+        }
+        public string GetName()
+        {
+            OnMethodCall("GetName() method called");
+            return this.Name;
+        }
+        public string GetDepartmentName()
+        {
+            OnMethodCall("GetDepartmentName() method called");
+            return this.DepartmentName;
+        }
+
+        public delegate void OnMethodCallDelegate(string message);
+        public event OnMethodCallDelegate MethodCallEvent;
+
+        protected virtual void OnMethodCall(string message)
+        {
+            MethodCallEvent?.Invoke(message);
+        }
+
+        public void UpdateEmployee(string Id)
+        {
+            this.Id = Id;
+        }
+        public void UpdateEmployee(string Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+        public void UpdateEmployee(string Id, string Name, string Departmentname)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.DepartmentName = Departmentname;
+        }
+    }
+
+}
---
+++ Assignment-1/Assignment-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+	 class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Employee Details");
+            Console.Write("\nEnter ID : ");
+            string id = Console.ReadLine();
+            Console.Write("Enter Name : ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Department Name : ");
+            string departmentName = Console.ReadLine();
+           
+            Employee e = new Employee(id, name, departmentName);
+
+            e.MethodCallEvent += EmployeeMethodCallEvent;
+
+            Console.WriteLine("\nID:{0} \nName:{1} \nDepartment Name:{2}", e.GetId(), e.GetName(), e.GetDepartmentName());
+            Console.ReadKey();
+        }
+
+        private static void EmployeeMethodCallEvent(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
